3cf12dc369ff54f41ee0080c96b78129b88e8e67
Nicolas Lafuente <lafuentenicolas1@gmail.com>
2025-06-30 12:05:25 +0200

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Brief_calculatrice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nombre1;
+            int nombre2;
+            int choix;
+            fonction.Affichage();
+            choix = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            Console.Write("Choissisez le premier nombre : ");
+            nombre1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(new string('-', 30));
+            Console.Write("Choissisez le second nombre : ");
+            nombre2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(new string('-', 30));
+
+            switch (choix)
+                {
+                    case 1:
+
+                        Console.Write("le produit est : ");
+                        fonction.Multiplication(nombre1, nombre2);
+                        break;
+
+                    case 2:
+                        Console.Write("la somme est : ");
+                        fonction.Addition(nombre1, nombre2);
+                        break;
+
+                    case 3:
+
+                        Console.Write("le quotient est : ");
+                        fonction.Division(nombre1, nombre2);
+                        break;
+
+                    case 4:
+                        Console.Write("le différence est : ");
+                        fonction.Soustraction(nombre1, nombre2);
+                        break;
+
+                }
+            }
+        }
+    
+    };
+
+
+
+
+
---
+++ fonction.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Brief_calculatrice
+{
+    internal class fonction
+    {
+       public static void Affichage()
+        {
+            Console.WriteLine(new string( '-', 30));
+            Console.WriteLine("1 - Multiplication");
+            Console.WriteLine("2 - Addition");
+            Console.WriteLine("3 - Division");
+            Console.WriteLine("4 - Soustraction");
+            Console.WriteLine(new string('-', 30));
+            Console.WriteLine("Choissisez une opération : ");
+            
+        }
+        public static void Multiplication(int nombre1, int nombre2)
+        {
+            
+            int multiplication = nombre1 * nombre2;
+            Console.WriteLine(multiplication);
+        }
+        public static void Addition(int nombre1, int nombre2)
+        {
+            int addition = nombre1 + nombre2;
+            Console.WriteLine(addition);
+        }
+        public static void Division(int nombre1, int nombre2)
+        {
+            int division = nombre1 / nombre2;
+            Console.WriteLine(division);
+            if (nombre1 / nombre2 == 0)
+            {
+                Console.WriteLine("erreur");
+            }
+        }
+        public static void Soustraction(int nombre1, int nombre2)
+        {
+            double soustraction = nombre1 - nombre2;
+            Console.WriteLine(soustraction);
+        }
+    }
+};
